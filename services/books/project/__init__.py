e61b7bbff5e5d0def88ef00c058a2d8499ca9f92
rafaelpardor <rafaelpardor@protonmail.com>
2021-12-05 15:08:57 -0500

    Formatting

--- services/books/project/__init__.py
+++ services/books/project/__init__.py
@@ -5,11 +5,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 def create_app(script_info=None):
     app = Flask(__name__)
     app_settings = os.getenv('APP_SETTINGS')
     app.config.from_object(app_settings)
-    
+
     db.init_app(app)
 
     from project.api.books import books_blueprint
@@ -17,4 +18,3 @@ def create_app(script_info=None):
     app.shell_context_processor({'app': app, 'db': db})
 
     return app
-
--- services/books/project/api/models.py
+++ services/books/project/api/models.py
@@ -1,5 +1,6 @@
 from project import db
 
+
 class Book(db.Model):
     __talbename__ = "book"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -9,11 +10,11 @@ class Book(db.Model):
     category = db.Column(db.String(128), nullable=False)
     publishDate = db.Column(db.String(128), nullable=False)
     description = db.Column(db.String(128), nullable=False)
-    
+
     def __init__(self, title, subtitle):
         self.title = title
         self.subtitle = subtitle
-    
+
     def to_json(self):
         return {
             'id': self.id,
