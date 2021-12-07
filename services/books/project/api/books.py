b915dc1f1e93c83a160b0cd837da83ac7843ffd5
rafaelpardor <rafaelpardor@protonmail.com>
2021-12-06 19:55:06 -0500

    Making search by any paramenter endpoint

--- services/books/project/api/books.py
+++ services/books/project/api/books.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, json, jsonify, request
+from flask import Blueprint, jsonify, request
 from sqlalchemy import exc
 
 from project.api.models import Book
@@ -15,13 +15,103 @@ def ping_pong():
     })
 
 
-@books_blueprint.route('/books/', methods=['GET'])
+@books_blueprint.route('/books', methods=['GET'])
 def get_books():
     if request.method == 'GET':
-        get_books = [book.to_json() for book in Book.query.all()]
-        response = {
+        try:
+            get_books = [book.to_json() for book in Book.query.all()]
+            response_object = {
+                "origin": "api_database",
+                "status": "success",
+                "data": get_books
+            }
+            return jsonify(response_object), 200
+        except ValueError:
+            response_object = {
+                "origin": "api_database",
+                "status": 500,
+                "message": "Failed to get books"
+            }
+            return response_object
+
+
+@books_blueprint.route('/books/<book_id>', methods=['GET'])
+def get_book_by_id(book_id):
+    try:
+        get_book = Book.query.filter_by(id=int(book_id)).first()
+
+        if not get_book:
+            response_object = {
+                "origin": "api_database",
+            }
+            response_object['message'] = "Book not found"
+            response_object['status'] = 404
+            response_object['data'] = []
+            return jsonify(response_object), 404
+
+        response_object = {
             "origin": "api_database",
-            "status": "success",
-            "data": get_books
+            "status": 200,
+            "data": {
+                "id": get_book.id,
+                "title": get_book.title
+            }
         }
-        return jsonify(response), 200
+        return jsonify(response_object), 200
+    except ValueError:
+        response_object = {
+            "origin": "api_database",
+            "status": 500,
+            "message": "Failed to get book"
+        }
+        return jsonify(response_object), 500
+
+
+@books_blueprint.route('/books/byany', methods=['GET'])
+def get_book_by_parameter():
+    try:
+        parameter = str(list(dict(request.args).keys())[0])
+        search = request.args.get(parameter)
+        search_book = db.session.query(Book).filter(
+            getattr(Book, parameter).ilike("%" + search + "%")
+        )
+        format_books = [book.to_json() for book in search_book]
+
+        if not search_book:
+            response_object = {
+                "origin": "api_database",
+            }
+            response_object['message'] = "Book not found"
+            response_object['status'] = 404
+            response_object['data'] = []
+            return jsonify(response_object), 404
+
+        response_object = {
+            "origin": "api_database",
+            "status": 200,
+            "data": format_books
+        }
+        return jsonify(response_object), 200
+    except TypeError:
+        response_object = {
+            "origin": "api_database",
+            "status": 500,
+            "message": "Failed to get book"
+        }
+        return jsonify(response_object), 500
+
+
+@books_blueprint.route('/books/', methods=['POST'])
+def create_book():
+    if request.method == 'POST':
+        pass
+
+
+@books_blueprint.route('/books/<id>', methods=['PUT'])
+def update_book():
+    pass
+
+
+@books_blueprint.route('/books/books', methods=['GET'])
+def delete_book():
+    pass
